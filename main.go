f6191aa1ca25f5bf184d3c21f937e749bc6ad37c
Shnifer <11@a.com>
2018-01-17 17:38:21 +0300

    6

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+func main() {
+	r := raspi.NewAdaptor()
+	led := gpio.NewLedDriver(r, "12")
+
+	work := func() {
+		gobot.Every(0.3*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("blinkBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{led},
+		work,
+	)
+
+	robot.Start()
+}
