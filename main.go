1f1f1abcaaaf9a63f7e5db556e3a5f55e0890692
Shnifer <11@a.com>
2018-01-19 10:13:34 +0300

    6

--- main.go
+++ main.go
@@ -6,18 +6,21 @@ import (
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
+	"fmt"
+	"strconv"
 )
 
 func main() {
+	d := 1000//milliseconds
 	r := raspi.NewAdaptor()
 	led := gpio.NewLedDriver(r, "12")
 
 	work := func() {
 		gobot.Every(2*time.Second, func() {
-			per:=time.After(time.Second)
+			per:=time.After(1*time.Second)
 			for{
 				select{
-					case <-time.After(10*time.Millisecond):
+					case <-time.After(d*time.Millisecond):
 						led.Toggle()
 					case <- per:
 						return
@@ -33,4 +36,18 @@ func main() {
 	)
 
 	robot.Start()
+	var str string
+	for {
+		fmt.Scanln(&str)
+		switch str {
+		case "quit", "exit":
+			robot.Stop()
+			return
+		default:
+			v,err:=strconv.Atoi(str)
+			if err==nil {
+				d=v
+			}
+		}
+	}
 }
